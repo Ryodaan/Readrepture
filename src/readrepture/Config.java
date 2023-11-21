081a116dc42976d7a231f7b8d51a9707f9f9ae1a
Fushigo <rizky23ska@gmail.com>
2023-11-21 20:55:01 +0700

    update

--- src/readrepture/Config.java
+++ src/readrepture/Config.java
@@ -17,7 +17,7 @@ public class Config {
      
     public static Connection configDB() throws SQLException {
         try {
-            String url = "jdbc:mysql://localhost:3306/hotelgalih";
+            String url = "jdbc:mysql://localhost:3306/";
             String user = "root";
             String pass = "";
             
